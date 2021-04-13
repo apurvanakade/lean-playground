3399440186af33c42fafaf487cb0e184398d28d0
Apurva Nakade <apurvnakade@gmail.com>
2021-04-13 10:23:14 -0400

    proofs of numeric_nat and numeric_omegac

---
+++ src/surreal/numeric_nat.lean
@@ -0,0 +1,15 @@
+import set_theory.surreal
+open pgame
+
+theorem numeric_nat (n : ℕ) : numeric n :=
+begin
+induction n with n hn,
+apply numeric_zero,
+apply numeric_add hn numeric_one,
+end
+
+theorem numeric_omega : numeric omega :=
+⟨by rintros ⟨ ⟩ ⟨ ⟩, λ i, by simp [numeric_nat i.down] , by rintros ⟨ ⟩⟩
+
+#check numeric_nat
+#check numeric_omega
