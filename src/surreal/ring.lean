d2de743440660e1467d395c238c74df85c100e96
Apurva Nakade <apurvnakade@gmail.com>
2021-04-21 19:45:14 -0400

    proofs of mul_zero and zero_mul

--- src/surreal/ring.lean
+++ src/surreal/ring.lean
@@ -1,21 +1,26 @@
 import set_theory.surreal 
--- set_option pp.notation false
-
-#check surreal
-#check pgame 
 
 namespace pgame
 
--- theorem mul_left_moves : Π {x y : pgame}, (x * y).left_moves = x.1 :=
-
-theorem zero_mul : ∀ (x : pgame), (0 * x).equiv 0 :=
-begin
-intros x,
-split,
-rw le_zero,
-sorry,
-rw zero_le,
-sorry,
-end
-
+def mul_zero_relabelling :
+  Π (x : pgame), relabelling (x * 0) 0
+  := by {rintros ⟨⟩, tidy}
+   
+def zero_mul_relabelling :
+  Π (x : pgame), relabelling (0 * x) 0
+  := by {rintros ⟨⟩, tidy}
+   
 end pgame
+
+
+
+
+
+
+
+
+
+
+
+
+
