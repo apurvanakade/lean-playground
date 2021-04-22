5558af392258b1581c826cf9866c1ee6c8c584eb
Apurva Nakade <apurvnakade@gmail.com>
2021-04-21 23:10:31 -0400

    more cleanup

--- src/surreal/ring.lean
+++ src/surreal/ring.lean
@@ -5,31 +5,32 @@ namespace pgame
 theorem mul_zero_relabelling : Π (x : pgame), relabelling (x * 0) 0
 | (mk xl xr xL xR) :=
 ⟨ 
-by { fsplit; rintro (⟨_, ⟨⟩⟩ | ⟨_, ⟨⟩⟩) },
-by { fsplit; rintro (⟨_, ⟨⟩⟩ | ⟨_, ⟨⟩⟩) }, 
-by { rintro (⟨_, ⟨⟩⟩ | ⟨_, ⟨⟩⟩) },
-by { rintro ⟨⟩ }
+by fsplit; rintro (⟨_, ⟨⟩⟩ | ⟨_, ⟨⟩⟩),
+by fsplit; rintro (⟨_, ⟨⟩⟩ | ⟨_, ⟨⟩⟩), 
+by rintro (⟨_, ⟨⟩⟩ | ⟨_, ⟨⟩⟩),
+by rintro ⟨⟩
 ⟩
    
 theorem zero_mul_relabelling : Π (x : pgame), relabelling (0 * x) 0
 | (mk xl xr xL xR) :=
 ⟨
-by { fsplit; rintro (⟨⟨⟩, _⟩ | ⟨⟨⟩, _⟩) },
-by { fsplit; rintro (⟨⟨⟩, _⟩ | ⟨⟨⟩, _⟩) },
-by { rintro (⟨⟨⟩,_⟩ | ⟨⟨⟩,_⟩) },
-by { rintro ⟨⟩ }
+by fsplit; rintro (⟨⟨⟩, _⟩ | ⟨⟨⟩, _⟩),
+by fsplit; rintro (⟨⟨⟩, _⟩ | ⟨⟨⟩, _⟩),
+by rintro (⟨⟨⟩,_⟩ | ⟨⟨⟩,_⟩),
+by rintro ⟨⟩
 ⟩ 
    
 
-theorem mul_one_relabelling :
-  Π (x : pgame), relabelling (x * (1:pgame)) x :=
+theorem mul_one_relabelling : Π (x : pgame), relabelling (x * (1:pgame)) x
+| (mk xl xr xL xR) :=
 begin 
-
-sorry
+refine ⟨_,_,_,_⟩,
+ 
+repeat {sorry},
 end
 
 
-
+end pgame
 
 
 
