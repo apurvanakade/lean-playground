d18bd3d3004e044eedafd7587aa59669247d5009
Apurva Nakade <apurvnakade@gmail.com>
2021-05-02 22:30:32 -0400

    New math.

--- src/surreal/mul_distrib.lean
+++ src/surreal/mul_distrib.lean
@@ -5,13 +5,13 @@ open pgame
 namespace pgame
 universe u
 
-meta def stupid_tactic : tactic unit :=
+meta def try_inl_inr : tactic unit :=
 `[assumption]
-    <|> (do `[apply sum.inr], stupid_tactic )
-    <|> (do `[apply sum.inl], stupid_tactic )
-    <|> (do `[apply prod.mk], stupid_tactic, stupid_tactic )
+    <|> (do `[apply sum.inr], try_inl_inr )
+    <|> (do `[apply sum.inl], try_inl_inr )
+    <|> (do `[apply prod.mk], try_inl_inr, try_inl_inr )
 
-lemma foo {x y : pgame} (L : x.left_moves ≃ y.left_moves) (R : x.right_moves ≃ y.right_moves)
+lemma equiv_of_mk_equiv {x y : pgame} (L : x.left_moves ≃ y.left_moves) (R : x.right_moves ≃ y.right_moves)
          (hl : ∀ (i : x.left_moves), (x.move_left i).equiv (y.move_left (L i)))
          (hr : ∀ (j : y.right_moves), (x.move_right (R.symm j)).equiv (y.move_right j)) :
        x.equiv y :=
@@ -34,14 +34,14 @@ end
 
 local infix ` ≈ ` := pgame.equiv
 
-lemma aux {a b c d e : pgame} : (a + b) + (c + d) - (e + b) ≈ a + c - e + d :=
+lemma left_distrib_aux {a b c d e : pgame} : (a + b) + (c + d) - (e + b) ≈ a + c - e + d :=
 begin
   apply @quotient.exact pgame,
   change (⟦a⟧ + ⟦b⟧ + (⟦c⟧ + ⟦d⟧) - (⟦e⟧ + ⟦b⟧) : game) = ⟦a⟧ + ⟦c⟧ + -⟦e⟧ + ⟦d⟧,
   abel,
 end
 
-lemma aux' {a b c d e : pgame} : (b + a) + (d + c) - (b + e) ≈ d + (a + c - e) :=
+lemma left_distrib_equiv_aux' {a b c d e : pgame} : (b + a) + (d + c) - (b + e) ≈ d + (a + c - e) :=
 begin
   apply @quotient.exact pgame,
   change (⟦b⟧ + ⟦a⟧ + (⟦d⟧ + ⟦c⟧) - (⟦b⟧ + ⟦e⟧) : game) = ⟦d⟧ + (⟦a⟧ + ⟦c⟧ + -⟦e⟧),
@@ -54,15 +54,15 @@ begin
   let x := mk xl xr xL xR,
   let y := mk yl yr yL yR,
   let z := mk zl zr zL zR,
-  refine foo _ _ _ _,
+  refine equiv_of_mk_equiv _ _ _ _,
   { fsplit,
-    { rintros (⟨_,(_|_)⟩|⟨_,(_|_)⟩), repeat {stupid_tactic}},
-    { rintros ((⟨_,_⟩|⟨_,_⟩)|(⟨_,_⟩|⟨_,_⟩)), repeat {stupid_tactic} },
+    { rintros (⟨_,(_|_)⟩|⟨_,(_|_)⟩), repeat {try_inl_inr}},
+    { rintros ((⟨_,_⟩|⟨_,_⟩)|(⟨_,_⟩|⟨_,_⟩)), repeat {try_inl_inr} },
     { rintros (⟨_,(_|_)⟩|⟨_,(_|_)⟩), repeat {refl} },
     { rintros ((⟨_,_⟩|⟨_,_⟩)|(⟨_,_⟩|⟨_,_⟩)), repeat {refl} }},
   { fsplit,
-    { rintros (⟨_,(_|_)⟩|⟨_,(_|_)⟩), repeat {stupid_tactic} },
-    { rintros ((⟨_,_⟩|⟨_,_⟩)|(⟨_,_⟩|⟨_,_⟩)), repeat {stupid_tactic} },
+    { rintros (⟨_,(_|_)⟩|⟨_,(_|_)⟩), repeat {try_inl_inr} },
+    { rintros ((⟨_,_⟩|⟨_,_⟩)|(⟨_,_⟩|⟨_,_⟩)), repeat {try_inl_inr} },
     { rintros (⟨_,(_|_)⟩|⟨_,(_|_)⟩), repeat {refl} },
     { rintros ((⟨_,_⟩|⟨_,_⟩)|(⟨_,_⟩|⟨_,_⟩)), repeat {refl} }},
   { rintros (⟨i,(j|k)⟩|⟨i,(j|k)⟩),
@@ -70,43 +70,43 @@ begin
         xL i * (y + z) + x * (yL j + z) - xL i * (yL j + z)
             ≈  (xL i * y + xL i * z) + (x * yL j + x * z) - (xL i * yL j + xL i * z)
             : by { refine add_congr (add_congr _ _) (neg_congr _); apply left_distrib_equiv }
-        ... ≈ xL i * y + x * yL j - xL i * yL j + x * z : aux },
+        ... ≈ xL i * y + x * yL j - xL i * yL j + x * z : left_distrib_aux },
     { calc
         xL i * (y + z) + x * (y + zL k) - xL i * (y + zL k)
             ≈ (xL i * y + xL i * z) + (x * y + x * zL k) - (xL i * y + xL i * zL k)
             : by { refine add_congr (add_congr _ _) (neg_congr _); apply left_distrib_equiv }
-        ... ≈  x * y + (xL i * z + x * zL k - xL i * zL k) : aux' },
+        ... ≈  x * y + (xL i * z + x * zL k - xL i * zL k) : left_distrib_equiv_aux' },
     { calc
         xR i * (y + z) + x * (yR j + z) - xR i * (yR j + z)
             ≈  (xR i * y + xR i * z) + (x * yR j + x * z) - (xR i * yR j + xR i * z)
             : by { refine add_congr (add_congr _ _) (neg_congr _); apply left_distrib_equiv }
-        ... ≈ xR i * y + x * yR j - xR i * yR j + x * z : aux },
+        ... ≈ xR i * y + x * yR j - xR i * yR j + x * z : left_distrib_aux },
     { calc
         xR i * (y + z) + x * (y + zR k) - xR i * (y + zR k)
             ≈ (xR i * y + xR i * z) + (x * y + x * zR k) - (xR i * y + xR i * zR k)
             : by { refine add_congr (add_congr _ _) (neg_congr _); apply left_distrib_equiv }
-        ... ≈ x * y + (xR i * z + x * zR k - xR i * zR k) : aux' } },
+        ... ≈ x * y + (xR i * z + x * zR k - xR i * zR k) : left_distrib_equiv_aux' } },
   { rintros ((⟨i,j⟩|⟨i,j⟩)|(⟨i,k⟩|⟨i,k⟩)),
     { calc
         xL i * (y + z) + x * (yR j + z) - xL i * (yR j + z)
             ≈  (xL i * y + xL i * z) + (x * yR j + x * z) - (xL i * yR j + xL i * z)
             : by { refine add_congr (add_congr _ _) (neg_congr _); apply left_distrib_equiv }
-        ... ≈ xL i * y + x * yR j - xL i * yR j + x * z : aux },
+        ... ≈ xL i * y + x * yR j - xL i * yR j + x * z : left_distrib_aux },
     { calc
         xR i * (y + z) + x * (yL j + z) - xR i * (yL j + z)
             ≈  (xR i * y + xR i * z) + (x * yL j + x * z) - (xR i * yL j + xR i * z)
             : by { refine add_congr (add_congr _ _) (neg_congr _); apply left_distrib_equiv }
-        ... ≈ xR i * y + x * yL j - xR i * yL j + x * z : aux },
+        ... ≈ xR i * y + x * yL j - xR i * yL j + x * z : left_distrib_aux },
     { calc
         xL i * (y + z) + x * (y + zR k) - xL i * (y + zR k)
             ≈ (xL i * y + xL i * z) + (x * y + x * zR k) - (xL i * y + xL i * zR k)
             : by { refine add_congr (add_congr _ _) (neg_congr _); apply left_distrib_equiv }
-        ... ≈  x * y + (xL i * z + x * zR k - xL i * zR k) : aux' },
+        ... ≈  x * y + (xL i * z + x * zR k - xL i * zR k) : left_distrib_equiv_aux' },
     { calc
         xR i * (y + z) + x * (y + zL k) - xR i * (y + zL k)
             ≈ (xR i * y + xR i * z) + (x * y + x * zL k) - (xR i * y + xR i * zL k)
             : by { refine add_congr (add_congr _ _) (neg_congr _); apply left_distrib_equiv }
-        ... ≈ x * y + (xR i * z + x * zL k - xR i * zL k) : aux' } }
+        ... ≈ x * y + (xR i * z + x * zL k - xR i * zL k) : left_distrib_equiv_aux' } }
 end
 using_well_founded { dec_tac := pgame_wf_tac }
 
